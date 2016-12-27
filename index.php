5fa807a52910213ddcb7bf00ec4bf664fb247dc5
mauricio pacheco <mauricio@guarapuava.com.br>
2016-12-27 15:04:44 -0200

    update index.php, indentations

--- index.php
+++ index.php
@@ -27,32 +27,25 @@ function adminlteMppFooter()
 {
    echo '<p id="created_by">Tema criado por <a href="http://example.com">Mauricio Paz Pacheco</a>.</p>';
 }
+
 /**
- * Echo the header description
+ * Show details of user
  * @return VOID
  */
-function adminlteMppHeader() 
+function user_details()
 {
-  add_options_page( 'My Plugin Options', 'My Plugin', 'manage_options', 'my-unique-identifier', 'my_plugin_options' );
-}
-
-function my_menu_pages()
-{
-	$current_user = wp_get_current_user();
+    $current_user = wp_get_current_user();
 
     add_menu_page('', 
-    			  '<img class="img-round" width="45" src="'.get_avatar_url($current_user->user_email).'"> <span class="auth-name">' . $current_user->user_firstname . '</span>', 
-    			  'manage_options', 
-    			  'profile.php', 
-    			  null,
-    			  '',
-    			  0 );
-    //add_submenu_page('my-menu', 'Submenu Page Title', 'Whatever You Want', 'manage_options', 'my-menu' );
+                  '<img class="img-round" width="45" src="'.get_avatar_url($current_user->user_email).'"> <span class="auth-name">' . $current_user->user_firstname . '</span>', 
+                  'manage_options', 
+                  'profile.php', 
+                  null,
+                  '',
+                  0 );
 }
 
 add_action('admin_enqueue_scripts', 'adminlteMppScripts');
 add_action('login_enqueue_scripts', 'adminlteMppScripts');
 add_action('admin_footer', 'adminlteMppFooter');
-add_action( 'admin_menu', 'adminlteMppHeader' );
-
-add_action('admin_menu', 'my_menu_pages');
+add_action('admin_menu', 'user_details');
