877408ff655cbf0d48182fc85ff9be37aa880297
mauricio pacheco <mauricio@guarapuava.com.br>
2016-12-27 14:59:03 -0200

    update readme

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ Plugin URI:
 Description: AdminLTE 2 Theme Wordpress adaptation (https://almsaeedstudio.com/themes/AdminLTE/index2.html)
 Author: Mauricio Paz Pacheco
 Version: 1.0
-Author URI: 
+Author URI: https://github.com/mauriciopazpp
 */
 
 /**
